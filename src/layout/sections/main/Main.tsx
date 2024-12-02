08bdc9cee87e91dd6462a366b0a3e258acca445d
DustyFlower <lukashevanatalia1992@gmail.com>
2024-12-02 18:32:40 +0100

    Layout of components

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,26 +2,49 @@ import React from 'react';
 import photo from '../../../assets/images/photo.jpg'
 import styled from 'styled-components';
 import {FlexWrapper} from '../../../components/FlexWrapper';
+import {Container} from '../../../components/Container';
+import {theme} from '../../../styles/Theme';
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={'center'} justify={'space-around'}>
-                <div>
-                    <span>Hi There</span>
-                    <Name>I am Svetlana Dyablo</Name>
-                    <MainTitle>A Web Developer.</MainTitle>
-                </div>
-                <Photo src={photo} alt="photo"/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper align={'center'} justify={'space-between'}>
+                    <div>
+                        <SmallText>Hi There</SmallText>
+                        <Name>I am <span>Svetlana Dyablo</span></Name>
+                        <MainTitle>A Web Developer.</MainTitle>
+                    </div>
+                    <PhotoWrapper>
+                        <Photo src={photo} alt="photo"/>
+                    </PhotoWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
 
     );
 };
 
-const StyledMain = styled.div`
+const StyledMain = styled.section`
     min-height: 100vh;
     background-color: #fff5e7;
+    display: flex;
+`
+const PhotoWrapper = styled.div`
+    position: relative;
+    z-index: 0;
+
+    &::before {
+        content: '';
+        width: 360px;
+        height: 470px;
+        border: 5px solid ${theme.colors.accent};
+
+        position: absolute;
+        top: -24px;
+        left: 24px;
+        z-index: -1;
+    }
 `
 
 const Photo = styled.img`
@@ -30,9 +53,37 @@ const Photo = styled.img`
     object-fit: cover;
 `
 const MainTitle = styled.h1`
-
+    font-size: 27px;
+    font-weight: 400;
 `
 
 const Name = styled.h2`
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 50px;
+    font-weight: 700;
+    letter-spacing: 0.05em;
+    margin: 10px 0;
+    
+    span {
+        position: relative;
+        z-index: 0;
+        
+        &::before {
+            content: '';
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.colors.accent};
+            
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+        }
+    }
+`
 
+const SmallText = styled.h2`
+    font-weight: 400;
+    font-size: 14px;
+    color: ${theme.colors.font}
 `
